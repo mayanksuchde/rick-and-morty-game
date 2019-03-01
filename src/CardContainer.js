b663294dc21d9a12835eb930bb9cbc20ad307f37
Andrew Drumonde <andrewdrumonde@Andrews-MacBook-Pro.local>
2019-03-01 12:01:53 -0500

    main logic working

---
+++ src/CardContainer.js
@@ -0,0 +1,80 @@
+import React, { Component } from 'react'
+import characters from './data';
+import Card from './Card';
+import './Cards.scss';
+export default class CardContainer extends Component {
+    constructor(){
+        super();
+        this.state={
+            characters:[]
+        }
+    }
+    componentDidMount(){
+        let shuffleArr=this.props.shuffle(characters)
+        this.setState({
+            characters:shuffleArr
+        })
+       
+    }
+    cardFlipper=(id)=>{
+
+        //we need to update an array:
+        //1>Copy Card array in State;
+        //go throuhg the array ,find the one we want and flip its is Clicked value to true or w
+        let newArr=this.state.characters;
+        let foundInd=newArr.findIndex((element)=>{ return id===element.id });
+        console.log(foundInd, newArr);
+        newArr[foundInd].isClicked=true;
+        this.setState({
+            characters:newArr
+        })
+        let filteredArray = this.clickCounter(this.state.characters)
+        let [element1, element2] = filteredArray; 
+
+        if (filteredArray.length === 2 ){
+            const characters = [...newArr];
+            let finalArr=[];
+            if (element1.name === element2.name){
+                finalArr=characters.map((obj)=>{
+                    if(obj.name===element1.name){
+                        obj.foundPair=true
+                    }
+                });
+                return finalArr;
+            }else{
+                finalArr=characters.map(obj=>{
+                    if(obj.id===element1.id || obj.id===element2.id){
+                        obj.isClicked=false;
+                    }
+                })
+            }
+            
+
+
+
+        }
+        }
+    clickCounter=(arr)=>{
+        let newArr = arr.filter((element)=>{ 
+            return element.isClicked === true && element.foundPair === false
+        })
+        return newArr;
+    }
+
+
+
+  render() {      
+      
+ 
+    return (
+      <div >
+        <h1>Cards</h1>
+        <div className='card-container'>
+            {this.state.characters.map((char,i)=>{ return <Card key={i} character={char} cardFlipper={this.cardFlipper} /> })}
+        </div>
+      </div>
+    )
+  }
+}
+
+// cardFlipper={this.cardFlipper}
---
+++ src/data.js
@@ -0,0 +1,54 @@
+const characters=[
+    {
+        id:'001' ,
+        name:'C137 Rick',
+        audio: '',
+        img : 'https://rickandmortyapi.com/api/character/avatar/1.jpeg',
+        isClicked:false,
+        foundPair: false
+
+    },
+    {
+        id:'002' ,
+        name:'Morty',
+        audio: '',
+        img:'https://rickandmortyapi.com/api/character/avatar/2.jpeg',
+        isClicked:false,
+        foundPair: false
+    },
+    {
+        id:'003' ,
+        name:'C137 Rick',
+        audio: '',
+        img : 'https://rickandmortyapi.com/api/character/avatar/1.jpeg',
+        isClicked:false,
+        foundPair: false
+    },
+    {
+        id:'004' ,
+        name:'Morty',
+        audio: '',
+        img:'https://rickandmortyapi.com/api/character/avatar/2.jpeg',
+        isClicked:false,
+        foundPair: false
+    },
+    {
+        id:'005' ,
+        name:'Snuffles',
+        audio: '',
+        img : 'https://rickandmortyapi.com/api/character/avatar/329.jpeg',
+        isClicked:false,
+        foundPair: false
+    },
+    {
+        id:'006' ,
+        name:'Snuffles',
+        audio: '',
+        img:'https://rickandmortyapi.com/api/character/avatar/329.jpeg',
+        isClicked:false,
+        foundPair: false
+    }
+
+    
+];
+export default characters;
